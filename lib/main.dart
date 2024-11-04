fd13669b1acf07f83e5fdfaebf429d9470f25fdd
Amir Berenji <a.berenji62@gmail.com>
2024-11-04 10:38:57 +0400

    check internet connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:petetco/commons/utils/app_style.dart';
 import 'package:petetco/features/splash/pages/splash_screen.dart';
 
@@ -9,19 +12,58 @@ void main() {
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
-
-  ]).then((_){
-
+  ]).then((_) {
     runApp(const ProviderScope(child: MyApp()));
-  
-  }
-
-  );
-  
+  });
 }
 
-class MyApp extends StatelessWidget {
+final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
+    GlobalKey<ScaffoldMessengerState>();
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final StreamSubscription<InternetStatus> _subscription;
+  late final AppLifecycleListener _listener;
+
+  @override
+  void initState() {
+    super.initState();
+    _subscription = InternetConnection().onStatusChange.listen((status) {
+      setState(() {
+        status == InternetStatus.connected;
+      });
+      if (status == InternetStatus.connected) {
+        print('Connected to the internet');
+      } else {
+        print('No internet connection');
+        scaffoldMessengerKey.currentState?.showSnackBar(
+          SnackBar(
+            backgroundColor: Styles.errorRed,
+            content: Text('No internet connection',style: Styles.headLineStyle2.copyWith(color: Styles.grey100),),
+            duration: const Duration(seconds: 10),
+          ),
+        );
+      }
+    });
+    _listener = AppLifecycleListener(
+      onResume: _subscription.resume,
+      onHide: _subscription.pause,
+      onPause: _subscription.pause,
+    );
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    _listener.dispose();
+    super.dispose();
+  }
 
   // This widget is the root of your application.
   @override
@@ -29,9 +71,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Petet Co',
+      scaffoldMessengerKey: scaffoldMessengerKey,
       theme: ThemeData.light(
-        useMaterial3: true,      
-      ).copyWith(primaryColor:primary),
+        useMaterial3: true,
+      ).copyWith(primaryColor: primary),
       darkTheme: ThemeData.light(),
       themeMode: ThemeMode.light,
       home: const SplashScreen(),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 import file_selector_macos
 import rive_common
 import shared_preferences_foundation
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
